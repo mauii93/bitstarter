38b23fef7123a11837a67ba75d3a2999dd8e7250
mauii93 <maurice.stam@student.uva.nl>
2013-07-08 21:20:52 +0000

    another try

--- web.js
+++ web.js
@@ -1,17 +1,19 @@
+
 var fs = require('fs');
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var content = fs.readFileSync(--dirname + '/index.html')
+var content = fs.readFileSync('index.html');
 
 
 app.get('/', function(request, response) {
-  response.end(content);
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
